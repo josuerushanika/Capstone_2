db8af98808b08c4aaba1b2008a757650b5649c3e
Frempong <samankwaa.frempong@gmail.com>
2023-02-21 15:18:21 +0000

    Add TV Show elements from API fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import logo from './assets/logo.png';
+import displayDOM from './modules/displayDOM.js';
 
 const logoImg = document.getElementById('logo');
 logoImg.src = logo;
+
+displayDOM();
