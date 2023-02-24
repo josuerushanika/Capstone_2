75041ae9e0c1c5148e42b38ddf43b16a90c71fbd
Odongo006 <www.henryodongo006@gmail.com>
2023-02-24 10:47:08 +0300

    Add refactored version

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,32 +1,19 @@
 import getData from './api.js';
-import {
-  sendComment, getAllComment,
-} from './fetchComment.js';
+import { sendComment, getAllComment } from './fetchComment.js';
 
-const clickClose = (container) => {
+const closePopup = (container) => {
   const cancelBtn = document.querySelector('.cancel');
   cancelBtn.addEventListener('click', () => {
     container.innerHTML = '';
   });
 };
 
-const fetchData = async () => {
-  const data = await getData();
-  const res = data.splice(150, 20);
-
+const renderPopup = (dataObj) => {
   const container = document.querySelector('.alert');
-  const btn = document.querySelectorAll('.comment');
-
-  btn.forEach((item) => {
-    item.addEventListener('click', (e) => {
-      const item = e.target.getAttribute('data-index');
-      const dataObj = res.find(
-        (resObj) => parseInt(resObj.id, 10) === parseInt(item, 10),
-      );
-      container.innerHTML += `
-          <div class="popup">
-          <div class="content">
-          <div class="part">
+  container.innerHTML += `
+    <div class="popup">
+      <div class="content">
+        <div class="part">
           <i class="fa-solid fa-x close fa-2x cancel" aria-hidden="true">x</i>
           <img class="popImage" src="${dataObj.image.medium}"/>
           <p class="namepop">${dataObj.name}</p>
@@ -34,9 +21,9 @@ const fetchData = async () => {
             <span class="commentspan"></span>
             <table class="table">
               <tr>
-              <td>username</td>
-              <td>comment</td>
-              <td>creation_date</td>
+                <td>username</td>
+                <td>comment</td>
+                <td>creation_date</td>
               </tr>
             </table>
           </div>
@@ -46,25 +33,37 @@ const fetchData = async () => {
             <textarea rows="5" id="usercomment" placeholder="Your comment" required></textarea>
             <button class="btnSubmit" id="item${dataObj.id}" type="submit">Comment</button>
           </form>
-          </div>
-          </div>
-          </div
-          `;
-      const form = document.querySelector('#form');
-      form.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const table = document.querySelector('.table');
-        const name = document.querySelector('#username');
-        const comment = document.querySelector('#usercomment');
-        const username = name.value;
-        const usercomment = comment.value;
-        await sendComment(parseInt(item, 10), username, usercomment);
-        table.innerHTML = '';
-        getAllComment(parseInt(item, 10));
-        form.reset();
-      });
-      getAllComment(parseInt(item, 10));
-      clickClose(container);
+        </div>
+      </div>
+    </div>
+  `;
+  const form = document.querySelector('#form');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const table = document.querySelector('.table');
+    const name = document.querySelector('#username');
+    const comment = document.querySelector('#usercomment');
+    const username = name.value;
+    const usercomment = comment.value;
+    await sendComment(parseInt(dataObj.id, 10), username, usercomment);
+    table.innerHTML = '';
+    getAllComment(parseInt(dataObj.id, 10));
+    form.reset();
+  });
+  getAllComment(parseInt(dataObj.id, 10));
+  closePopup(container);
+};
+
+const fetchData = async () => {
+  const data = await getData();
+  const res = data.splice(150, 20);
+
+  const btn = document.querySelectorAll('.comment');
+  btn.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const index = e.target.getAttribute('data-index');
+      const dataObj = res.find((resObj) => parseInt(resObj.id, 10) === parseInt(index, 10));
+      renderPopup(dataObj);
     });
   });
 };
