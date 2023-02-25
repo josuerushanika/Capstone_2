9d40e22a8a165aaa85f67792fcbb7bbd8ea7ab18
Frempong <samankwaa.frempong@gmail.com>
2023-02-25 14:07:19 +0000

    Made more review changes

--- src/modules/totalComment.js
+++ src/modules/totalComment.js
@@ -2,7 +2,7 @@ const commentCount = (Table, span) => {
   const table = document.querySelector('.table');
   const rows = table.querySelectorAll('tr');
   const count = rows.length - 1;
-  span.innerText = `${rows.length - 1} comments for this Show`;
+  span.innerText = `${count} comments for this Show`;
   return count;
 };
 
